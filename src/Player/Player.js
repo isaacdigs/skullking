d5fe5d23f82e589fd962762c318be5b55fa9629b
isaacdigs <pk07057@gmail.com>
2020-02-20 04:26:49 +0900

    added styles and made a cancel button

--- src/Player/Player.js
+++ src/Player/Player.js
@@ -10,6 +10,8 @@ export default function Player() {
     padding: "3px",
     margin: "10px",
     border: "1px solid black",
+    boxShadow: "5px 5px rgba(0, 0, 0, 0.5)",
+    backgroundColor: "rgba(255, 255, 255, 0.8)"
   }
 
   //state variables
@@ -18,6 +20,7 @@ export default function Player() {
   const [currentTrickGuess, setCurrentTrickGuess] = useState(null);
   const [currentBonus, setCurrentBonus] = useState(null);
   const [tricksWon, setTricksWon] = useState(null);
+  const [previousScore, setPreviousScore] = useState(null);
   let currentScore;
   if (currentTrickGuess !== 0 && currentTrickGuess === tricksWon) {
     currentScore = currentTrickGuess * 20 + currentBonus;
@@ -29,7 +32,8 @@ export default function Player() {
     currentScore = -(Math.abs(currentTrickGuess - tricksWon)*10)
   } else {
     currentScore = null;
-  }
+  };
+  
 
   //handling finish
   const [done, setDone] = useState(false);
@@ -48,9 +52,9 @@ export default function Player() {
 
   return (
     <div style={style}>
-      <input type="text" style={{fontWeight: "bold", textAlign: "center", fontSize: "2em", border: "none", width: "90%"}}></input>
+      <input type="text" style={{fontWeight: "bold", textAlign: "center", fontSize: "1.6em", border: "none", width: "70%"}}></input>
       <hr />
-      <p style={{fontWeight: "bold", backgroundColor: "#ddd", fontSize: "1em", margin: "0"}}>총 점수: <p style={{fontSize:"2em", margin: "0"}}>{totalScore}</p></p>
+      <p style={{fontWeight: "bold", fontSize: "1em", margin: "0"}}>총 점수: <p style={{fontSize:"2em", margin: "0"}}>{totalScore}</p></p>
       <hr />
       <p style={{fontWeight: "bold"}}> {currentRound} {currentRound !== "끝" ? "라운드" : null} </p>
       <hr />
@@ -84,38 +88,38 @@ export default function Player() {
       <p style={{fontSize: "0.8em", margin: "0"}}>보너스:
         <Popup
           trigger={<div className="menu-item"> {currentBonus} </div>}
-          position="bottom"
+          position="top"
           on="click"
-          closeOnDocumentClick
+          
           mouseLeaveDelay={300}
           mouseEnterDelay={0}
           contentStyle={{ padding: "0px", border: "none" }}
           arrow={false}
         >
 
-          <div className="menu">
-          <div className="menu-item" onClick={() => setCurrentBonus( -10 )}>-20</div>
-            <div className="menu-item" onClick={() => setCurrentBonus( -10 )}>-10</div>
-            <div className="menu-item" onClick={() => setCurrentBonus( 0 )}>0</div>
-            <div className="menu-item" onClick={() => setCurrentBonus( 10 )}>10</div>
-            <div className="menu-item" onClick={() => setCurrentBonus( 20 )}>20</div>
-            <div className="menu-item" onClick={() => setCurrentBonus( 30 )}>30</div>
-            <div className="menu-item" onClick={() => setCurrentBonus( 40 )}>40</div>
-            <div className="menu-item" onClick={() => setCurrentBonus( 50 )}>50</div>
-            <div className="menu-item" onClick={() => setCurrentBonus( 60 )}>60</div>
-            <div className="menu-item" onClick={() => setCurrentBonus( 70 )}>70</div>
-            <div className="menu-item" onClick={() => setCurrentBonus( 80 )}>80</div>
-            <div className="menu-item" onClick={() => setCurrentBonus( 90 )}>90</div>
-            <div className="menu-item" onClick={() => setCurrentBonus( 100 )}>100</div>
-            <div className="menu-item" onClick={() => setCurrentBonus( 110 )}>110</div>
-            <div className="menu-item" onClick={() => setCurrentBonus( 120 )}>120</div>
+          <div className="menu bonus-menu">
+            <div className="menu-item bonus-menu-item" onClick={() => setCurrentBonus( -10 )}>-20</div>
+            <div className="menu-item bonus-menu-item" onClick={() => setCurrentBonus( -10 )}>-10</div>
+            <div className="menu-item bonus-menu-item" onClick={() => setCurrentBonus( 0 )}>0</div>
+            <div className="menu-item bonus-menu-item" onClick={() => setCurrentBonus( 10 )}>10</div>
+            <div className="menu-item bonus-menu-item" onClick={() => setCurrentBonus( 20 )}>20</div>
+            <div className="menu-item bonus-menu-item" onClick={() => setCurrentBonus( 30 )}>30</div>
+            <div className="menu-item bonus-menu-item" onClick={() => setCurrentBonus( 40 )}>40</div>
+            <div className="menu-item bonus-menu-item" onClick={() => setCurrentBonus( 50 )}>50</div>
+            <div className="menu-item bonus-menu-item" onClick={() => setCurrentBonus( 60 )}>60</div>
+            <div className="menu-item bonus-menu-item" onClick={() => setCurrentBonus( 70 )}>70</div>
+            <div className="menu-item bonus-menu-item" onClick={() => setCurrentBonus( 80 )}>80</div>
+            <div className="menu-item bonus-menu-item" onClick={() => setCurrentBonus( 90 )}>90</div>
+            <div className="menu-item bonus-menu-item" onClick={() => setCurrentBonus( 100 )}>100</div>
+            <div className="menu-item bonus-menu-item" onClick={() => setCurrentBonus( 110 )}>110</div>
+            <div className="menu-item bonus-menu-item" onClick={() => setCurrentBonus( 120 )}>120</div>
           </div>
         </Popup>
       </p>
       <p style={{fontSize: "0.8em", margin: "0"}}>실제 트릭:
         <Popup
           trigger={<div className="menu-item"> {tricksWon} </div>}
-          position="bottom"
+          position="top"
           on="click"
           closeOnDocumentClick
           mouseLeaveDelay={300}
@@ -144,6 +148,7 @@ export default function Player() {
       <button 
         onClick={() => {
           setTotalScore(totalScore + currentScore);
+          setPreviousScore(currentScore);
           currentScore = 0;
           setCurrentTrickGuess(null);
           setCurrentBonus(null);
@@ -160,6 +165,19 @@ export default function Player() {
       style={{fontSize: "1em", fontWeight: "bold", boxShadow: "2px 2px #eee"}}
       > 
         {done === false ? "적용" : "리셋"}
+      </button>
+      <button
+        style={{fontSize: "1em", fontWeight: "bold", boxShadow: "2px 2px #eee"}}  
+        onClick={() => {
+            if (previousScore !== null) {
+              setTotalScore(totalScore - previousScore);
+              setPreviousScore(null);
+              setCurrentRound(currentRound - 1);
+            }
+          }
+        }
+      >
+        취소
       </button>
     </div>
   )
